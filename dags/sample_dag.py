b1f3901486ad918c7883c769cb4a69236291c650
TorSatherley <torsatherley@hotmail.co.uk>
2025-05-21 16:25:26 +0100

    adds the python logic for transforming and calling the loaded data, and also adds the dependencies for the workflow

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -10,3 +10,73 @@ with DAG(
     render_template_as_native_obj=True
 ) as dag:
     pass
+
+
+# Import the PythonOperator
+from airflow.operators.python import PythonOperator
+
+
+def extract_data_callable():
+    # Print message, return a response
+    print("Extracting data from an weather API")
+    return {
+        "date": "2023-01-01",
+        "location": "NYC",
+        "weather": {
+            "temp": 33,
+            "conditions": "Light snow and wind"
+        }
+    }
+
+
+extract_data = PythonOperator(
+    dag=dag,
+    task_id="extract_data",
+    python_callable=extract_data_callable
+)
+
+
+# Import pandas
+import pandas as pd
+
+
+def transform_data_callable(raw_data):
+    # Transform response to a list
+    transformed_data = [
+        [
+            raw_data.get("date"),
+            raw_data.get("location"),
+            raw_data.get("weather").get("temp"),
+            raw_data.get("weather").get("conditions")
+        ]
+    ]
+    return transformed_data
+
+
+transform_data = PythonOperator(
+    dag=dag,
+    task_id="transform_data",
+    python_callable=transform_data_callable,
+    op_kwargs={"raw_data": "{{ ti.xcom_pull(task_ids='extract_data') }}"}
+)
+
+def load_data_callable(transformed_data):
+    # Load the data to a DataFrame, set the columns
+    loaded_data = pd.DataFrame(transformed_data)
+    loaded_data.columns = [
+        "date",
+        "location",
+        "weather_temp",
+        "weather_conditions"
+    ]
+    print(loaded_data)
+
+
+load_data = PythonOperator(
+    dag=dag,
+    task_id="load_data",
+    python_callable=load_data_callable,
+    op_kwargs={"transformed_data": "{{ ti.xcom_pull(task_ids='transform_data') }}"}
+)
+
+extract_data >> transform_data >> load_data
