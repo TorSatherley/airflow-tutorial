1cb3c40b9ecf04e3e55e33cf5c83149f1fab3f80
TorSatherley <torsatherley@hotmail.co.uk>
2025-05-21 16:32:56 +0100

    converts the sample_dag.py code using TaskFlow API to improve readability and simplicity

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -1,82 +1,59 @@
-from airflow import DAG
+from airflow.decorators import dag, task
 from datetime import datetime
-
-with DAG(
-    dag_id="weather_etl",
-    start_date=datetime(year=2024, month=1, day=1, hour=9, minute=0),
-    schedule="@daily",
-    catchup=True,
-    max_active_runs=1,
-    render_template_as_native_obj=True
-) as dag:
-    pass
-
-
-# Import the PythonOperator
-from airflow.operators.python import PythonOperator
-
-
-def extract_data_callable():
-    # Print message, return a response
-    print("Extracting data from an weather API")
-    return {
-        "date": "2023-01-01",
-        "location": "NYC",
-        "weather": {
-            "temp": 33,
-            "conditions": "Light snow and wind"
-        }
-    }
-
-
-extract_data = PythonOperator(
-    dag=dag,
-    task_id="extract_data",
-    python_callable=extract_data_callable
-)
-
-
-# Import pandas
 import pandas as pd
 
 
-def transform_data_callable(raw_data):
-    # Transform response to a list
-    transformed_data = [
-        [
-            raw_data.get("date"),
-            raw_data.get("location"),
-            raw_data.get("weather").get("temp"),
-            raw_data.get("weather").get("conditions")
+@dag(
+    start_date=datetime(year=2023, month=1, day=1, hour=9, minute=0),
+    schedule="@daily",
+    catchup=True,
+    max_active_runs=1
+)
+def weather_etl():
+    @task()
+    def extract_data():
+        # Print message, return a response
+        print("Extracting data from an weather API")
+        return {
+            "date": "2023-01-01",
+            "location": "NYC",
+            "weather": {
+                "temp": 33,
+                "conditions": "Light snow and wind"
+            }
+        }
+
+    @task()
+    def transform_data(raw_data):
+        # Transform response to a list
+        transformed_data = [
+            [
+                raw_data.get("date"),
+                raw_data.get("location"),
+                raw_data.get("weather").get("temp"),
+                raw_data.get("weather").get("conditions")
+            ]
         ]
-    ]
-    return transformed_data
+        return transformed_data
+
+    @task()
+    def load_data(transformed_data):
+        # Load the data to a DataFrame, set the columns
+        loaded_data = pd.DataFrame(transformed_data)
+        loaded_data.columns = [
+            "date",
+            "location",
+            "weather_temp",
+            "weather_conditions"
+        ]
+        print(loaded_data)
+
+    # Set dependencies using function calls
+    raw_dataset = extract_data()
+    transformed_dataset = transform_data(raw_dataset)
+    load_data(transformed_dataset)
 
 
-transform_data = PythonOperator(
-    dag=dag,
-    task_id="transform_data",
-    python_callable=transform_data_callable,
-    op_kwargs={"raw_data": "{{ ti.xcom_pull(task_ids='extract_data') }}"}
-)
+# Allow the DAG to be run
+weather_etl()
 
-def load_data_callable(transformed_data):
-    # Load the data to a DataFrame, set the columns
-    loaded_data = pd.DataFrame(transformed_data)
-    loaded_data.columns = [
-        "date",
-        "location",
-        "weather_temp",
-        "weather_conditions"
-    ]
-    print(loaded_data)
-
-
-load_data = PythonOperator(
-    dag=dag,
-    task_id="load_data",
-    python_callable=load_data_callable,
-    op_kwargs={"transformed_data": "{{ ti.xcom_pull(task_ids='transform_data') }}"}
-)
-
-extract_data >> transform_data >> load_data
